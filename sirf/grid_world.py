f5fdb55507072557eede52ef480bdb732d4b5fb0
Craig Corcoran <ccor@cs.utxas.edu>
2012-07-31 19:54:47 -0500

    refactored sirf gradient code to work with cg - still in the works

--- sirf/grid_world.py
+++ sirf/grid_world.py
@@ -1,5 +1,6 @@
 import numpy
 import scipy.sparse
+import scipy.optimize
 from random import choice
 import matplotlib.pyplot as plt
 
@@ -148,8 +149,9 @@ class SIRFObjectiveFn:
     
     def __init__(self, W, rewards, states, env, mod_wt):
         
-        self.W = W
-        
+        self.W = scipy.sparse.csc_matrix(W)
+        self.mod_wt = mod_wt
+
         rewards = rewards - numpy.mean(rewards) # worth loss is sparsity to center?
         rewards = rewards / numpy.sqrt(numpy.mean(rewards**2)) # normalize std to one
         assert (rewards.std() - 1) < 1e-8
@@ -159,7 +161,7 @@ class SIRFObjectiveFn:
         self.n_states, self.n_features = W.shape
         
         # create tabular feature data matrices given the sampled states
-        X_all = scipy.sparse.lil_matrix((self.n_samples+1, W.shape[0]), dtype = numpy.int)
+        X_all = scipy.sparse.lil_matrix((self.n_samples+1, self.n_states), dtype = numpy.int)
         for j in xrange(self.n_samples+1):
             X_all[j, env.state_to_index(states[j])] = 1
         X_all[:,-1] = 1 # append constant vector
@@ -170,7 +172,8 @@ class SIRFObjectiveFn:
 
         self._precompute()
         
-    def _precompute():
+    def _precompute(self):
+
         self.PHI = self.X * self.W
         self.PHI_p = self.X_p * self.W
         self.E = self.PHI.T * self.PHI
@@ -182,12 +185,14 @@ class SIRFObjectiveFn:
 
         # create diag matrix used to normalize each feature's variance to 1: TODO use diag?
         STD = scipy.sparse.csc_matrix.mean(self.PHI.multiply(self.PHI), axis = 0)
-        STD = numpy.sqrt(STD)
-        D = scipy.sparse.dia_matrix((1./STD, 0), (n_features, n_features))
+        STD = numpy.sqrt(STD) + 1e-12
+        D = scipy.sparse.dia_matrix((1./STD, 0), (self.n_features, self.n_features))
         
+        print self.PHI_p.shape
+        print D.shape
         self.PHI_p_norm = self.PHI_p * D
 
-    def _reward_gradient():
+    def _reward_gradient(self):
         
         B = self.R.T * self.PHI
         dW_rew = self.X.T * (self.R * (B * self.E) + (self.PHI * B.T) * B \
@@ -198,7 +203,7 @@ class SIRFObjectiveFn:
         #assert numpy.linalg.norm((dW_rew - dW_rew_old).todense()) < 1e-4
         return dW_rew
 
-    def _model_gradient():
+    def _model_gradient(self):
         
         # old update using chain rule
         #E_p = PHI_p_norm.T * PHI
@@ -212,8 +217,11 @@ class SIRFObjectiveFn:
                (self.PHI * B.T) * B - 2 * self.PHI_p_norm * B)) \
                * (1./self.n_features)
 
-    def get_gradient(W):
+    def get_gradient(self, W):
         
+        print 'gradient'
+
+        W = scipy.sparse.csc_matrix(numpy.reshape(W, (self.n_states, self.n_features)))
         if not (W.data == self.W.data).all(): # too slow for inner loop?
             self.W = W
             self._precompute()
@@ -221,10 +229,13 @@ class SIRFObjectiveFn:
         dW_rew = (1./self.n_samples) * self._reward_gradient()
         dW_mod = (1./self.n_samples) * self._model_gradient()
         
-        return dW_rew + self.mod_wt * dW_mod 
+        return (dW_rew + self.mod_wt * dW_mod).todense().flatten() 
 
-    def get_loss(W):
+    def get_loss(self, W):
         
+        print 'loss'
+
+        W = scipy.sparse.csc_matrix(numpy.reshape(W, (self.n_states, self.n_features)))
         if not (W.data == self.W.data).all():
             self.W = W
             self._precompute()
@@ -239,31 +250,49 @@ class SIRFObjectiveFn:
 
         return (1./self.n_samples) * (r_loss + self.mod_wt * m_loss)
 
-def init_grid_world():
+def init_mdp(goals = None, walls_on = False, size = 9):
+
     if goals is None:
         #goals = numpy.array([walls.shape]) / 2 # single goal ixn center
         goals = numpy.array([[2,2], [2,6], [6,2], [6,6]]) # four goals in the corners 
     
-    walls[:,4] = 1
-    walls[4,:] = 1
+    walls = numpy.zeros((size,size))
+    if walls_on:
+        walls[:, size/2 + 1] = 1
+        walls[size/2 + 1, :] = 1
+
     grid_world = GridWorld(walls, goals)
 
     rand_policy = RandomPolicy()
 
     mdp = MDP(grid_world, rand_policy)
-    
-    n_states = numpy.prod(walls.shape) 
 
-    W = numpy.random.standard_normal((n_states, n_features))
+    return mdp
+
+def init_weights(shape):
+
+    W = numpy.random.standard_normal(shape)
     inv_norms = numpy.diag(1./numpy.sqrt(numpy.sum(W**2, axis=0)))
     W = numpy.dot(W, inv_norms )
-    #W[-1, :] = 0 # set bias to zero initialy
     W = scipy.sparse.csc_matrix(W)
 
-def cg_main():
+    return W
+
+def cg_main(n_samples = 250, size = 9, goals = None, \
+        n_features = 8, mod_wt = 8):
     
+    mdp = init_mdp()
+    env = mdp.environment
+    W = init_weights((size**2, n_features)).todense()
 
+    n_iters = 10
+    for i in xrange(n_iters):
+        
+        states, rewards = mdp.sample(n_samples)
+        obj = SIRFObjectiveFn(W, rewards, states, env, mod_wt)
+        print scipy.optimize.fmin_cg(obj.get_loss, W, fprime = obj.get_gradient)
 
+    
 def main(n_samples = 250, walls = numpy.zeros((9,9)), goals = None, \
         n_features = 8, mod_wt = 8): #, lr = 1e-2, total_decay = 1e-1):
 
@@ -411,7 +440,7 @@ def test_grid_world():
 
 
 if __name__ == "__main__":
-    main()
+    cg_main()
 
 
             
