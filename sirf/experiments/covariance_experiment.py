7f298659e1fef8b36666dd4396b45d6a1bcaf48b
Leif Johnson <leif@leifjohnson.net>
2013-02-11 10:10:19 -0600

    Tweak output during experiment, and add regularization command line options.

--- sirf/experiments/covariance_experiment.py
+++ sirf/experiments/covariance_experiment.py
@@ -16,18 +16,20 @@ import util
 # mark shifts and best theta
 
 @plac.annotations(
-    k=('number of features', 'option', 'k', int),
+    k=('number of features', 'option', None, int),
     env_size=('size of the grid world', 'option', 's', int),
     lam=('lambda for TD-lambda training', 'option', None, float),
-    gam=('discount factor', 'option', 'g', float),
+    gam=('discount factor', 'option', None, float),
     beta=('covariance loss paramter', 'option', None, float),
     eps=('epsilon for computing TD-lambda horizon', 'option', None, float),
-    patience=('train until patience runs out', 'option', 'p', int),
+    patience=('train until patience runs out', 'option', None, int),
     max_iter=('train for at most this many iterations', 'option', 'i', int),
     weighting=('method for sampling from grid world', 'option', None, str, ['policy', 'uniform']),
+    l1theta=('regularize theta with this L1 parameter', 'option', None, float),
+    l1code=('regularize feature code with this L1 parameter', 'option', None, float),
     loss_types=('train on these losses in this order', 'option', None, str),
     grad_vars=('compute gradient using these variables', 'option', None, str),
-    nonlin=('feature nonlinearity', 'option', 'n', str, ['sigmoid', 'relu']),
+    nonlin=('feature nonlinearity', 'option', None, str, ['sigmoid', 'relu']),
     nonzero=('penalty for zero theta vectors', 'option', None, float),
     )
 def main(k = 16,
@@ -39,8 +41,9 @@ def main(k = 16,
          partition = None,
          patience = 15,
          max_iter = 15,
-         weighting = 'policy', 
-         reg = ('l1-theta', 5e-2), 
+         weighting = 'uniform', 
+         l1theta = None,
+         l1code = None,
          loss_types = 'covariance bellman',
          grad_vars = 'model',
          nonlin = None,
@@ -59,6 +62,11 @@ def main(k = 16,
     m.gam = gam
 
     print 'constructing basis'
+    reg = None
+    if l1theta is not None:
+        reg = ('l1-theta', l1theta)
+    if l1code is not None:
+        reg = ('l1-code', l1code)
     bb = Basis(n, k, beta_ratio, partition = partition, reg_tuple = reg, nonlin = nonlin, nonzero = nonzero)
     bb.theta[:,-1] = m.R # initialize the last column as the reward function
 
@@ -116,24 +124,31 @@ def main(k = 16,
         except KeyboardInterrupt:
             print '\n user stopped current training loop'
 
+    def output(prefix, suffix='pdf'):
+        return '%s.k=%i.reg=%s.lam=%s.gam=%s.b=%s.%s%s.%s' % (
+            prefix, k,
+            str(reg) + '+',
+            lam, gam, beta, weighting,
+            '.nonlin=%s' % nonlin if nonlin else '',
+            suffix)
+
     # save results!
-    out_path = 'covariance_results.k=%i.reg=%s.lam=%s.%s.size=%i.pickle.gz' % (k, str(reg), str(lam), weighting, env_size)
-    with util.openz(out_path, "wb") as out_file:
+    with util.openz(output('covariance_results', 'pickle.gz'), "wb") as out_file:
         pickle.dump((test_loss, test_be, true_be, true_lsq), out_file, protocol = -1)
 
     # plot basis functions
     plot_features(bb.theta)
-    plt.savefig('basis.k=%i.reg=%s.lam=%s.b=%s.%s.pdf' % (k, str(reg), str(lam), str(beta), weighting)) # add gam etc to string?
+    plt.savefig(output('basis'))
     
     # plot learning curves
     plot_learning_curves(numpy.array([test_loss, test_be, true_be, true_lsq]), 
                          ['test loss', 'test BE', 'true BE', 'true lsq'],
                          ['r-','g-','b-','k-'])
-    plt.savefig('loss.k=%i.reg=%s.lam=%s.b=%s.%s.pdf' % (k, str(reg), str(lam), str(beta), weighting))
+    plt.savefig(output('loss'))
     
     # plot value functions
     plot_value_functions(env_size, m, bb)
-    plt.savefig('value.k=%i.reg=%s.lam=%s.b=%s.%s.pdf' % (k, str(reg), str(lam), str(beta), weighting))
+    plt.savefig(output('value'))
 
 
 def plot_value_functions(size, m, b):
@@ -173,11 +188,11 @@ def plot_learning_curves(losses, labels, draw_styles = ['r-','g-','b-','k-']):
     print losses, labels, draw_styles
     for i, l in enumerate(losses):
         x = range(len(l))
-        ax.plot(x, l / l.max(), draw_styles[i], label=labels[i])
+        ax.plot(x, l / l.mean() / 3, draw_styles[i], label=labels[i])
 
     plt.ylim(0, 1)
     plt.title('Normalized Losses per CG Minibatch')
-    plt.legend()
+    plt.legend(loc='lower left')
     #handles, labels = ax.get_legend_handles_labels()
     #ypos = numpy.array([numpy.zeros_like(switch), numpy.ones_like(switch)])
     #ax.plot(numpy.array([switch,switch]), ypos, 'k-') # draw switch positions and best theta
@@ -191,11 +206,13 @@ def train_basis(basis, model, S, R, S_test, R_test, Mphi, Mrew, patience,
     true_be = numpy.append(numpy.array([]), model.bellman_error(basis.theta, weighting = weighting))
     true_lsq = numpy.append(numpy.array([]), model.value_error(basis.theta, weighting = weighting))
 
+    it = 0
     n_test_inc = 0
     best_test_loss = numpy.inf
     try:
         while (n_test_inc < patience):
-            
+            it += 1
+            print '*** iteration', it, '***'
             old_theta = copy.deepcopy(basis.theta)
             basis.set_theta( fmin_cg(basis.loss, basis.theta.flatten(), basis.grad,
                                 args = (S, R, Mphi, Mrew),
@@ -205,7 +222,9 @@ def train_basis(basis, model, S, R, S_test, R_test, Mphi, Mrew, patience,
             delta = numpy.linalg.norm(old_theta-basis.theta)
             print 'delta theta: ', delta
 
-            print 'normalizing columns; pre norms: ', numpy.apply_along_axis(numpy.linalg.norm, 0, basis.theta).mean()
+            norms = numpy.apply_along_axis(numpy.linalg.norm, 0, basis.theta)
+            print 'column norms: %.2f min / %.2f avg / %.2f max' % (
+                norms.min(), norms.mean(), norms.max())
             basis.theta = numpy.apply_along_axis(lambda v: v/numpy.linalg.norm(v), 0, basis.theta)
             
             err = basis.loss(basis.theta, S_test, R_test, Mphi, Mrew) #basis.loss_be(basis.theta, S_test, Sp_test, R_test)  
